60e74ed2e46b65fe7fb21e9bc610487aa3883067
gagahputrabangsa <91167860+gagahputrabangsa@users.noreply.github.com>
2021-10-19 22:16:14 +0700

    Update lkom4.2.py

--- lkom4.2.py
+++ lkom4.2.py
@@ -7,15 +7,14 @@ aku=0
 while aku==0:
     k=input('masukan list angka: ')
     # nilai di dalam K yang diberi spasi # 3 5 9
-    genap=[]
-    ganjil=[]
+    
     for k in k.split():
         if int(k)%2==0:
-                genap.append(int(k))
+                
                 print(' terdapat bilangan genap')
                 break
         elif int(k)%2==0:
-            genap.append(int(k))
+            
             if int(k)%2==0:
                     print('terdapat bilangan genap')
                     break
@@ -25,7 +24,7 @@ while aku==0:
             
         else: 
             if int(k)%2==0:
-                genap.append(int(k))
+                
                 print(' terdapat bilangan genap')
 
             
